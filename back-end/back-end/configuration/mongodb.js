5afe89e3f897d0497d9e13db20aac15553bb530c
canino_jories <canino_jories@hotmail.com>
2015-01-29 20:44:56 +0800

    adding uri for mongolab

--- back-end/back-end/configuration/mongodb.js
+++ back-end/back-end/configuration/mongodb.js
@@ -3,8 +3,7 @@
 
   module.exports = function(node, dbName) {
     if (node.mongoose.connection.readyState !== 1) {
-      console.log( process.env.MONGOLAB_URI );
-      var db = node.Promise.all([node.mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/' + dbName)]);
+      var db = node.Promise.all([node.mongoose.connect('mongodb://rappler:haurappler@ds039431.mongolab.com:39431/rappler' || 'mongodb://localhost:27017/' + dbName)]);
       return db;
     } else {
       return node.Promise.all([node.mongoose]);
