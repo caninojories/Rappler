f61daeb428c0ad8853fe8412957ecd81abcb48a3
canino_jories <canino_jories@hotmail.com>
2015-01-29 18:03:50 +0800

    mongolab

--- back-end/back-end/configuration/mongodb.js
+++ back-end/back-end/configuration/mongodb.js
@@ -3,6 +3,7 @@
 
   module.exports = function(node, dbName) {
     if (node.mongoose.connection.readyState !== 1) {
+      console.log( MONGOLAB_URI );
       var db = node.Promise.all([node.mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/' + dbName)]);
       return db;
     } else {
