d218d64d1685f395e717252fb0fd4038fd1bd704
canino_jories <canino_jories@hotmail.com>
2015-01-29 16:34:29 +0800

    first commit

---
+++ back-end/back-end/server.js
@@ -0,0 +1,56 @@
+(function() {
+  'use strict';
+
+  global.app_require = function(name) {
+    return require( __dirname + '/' + name );
+  };
+
+  var node      = app_require( 'services/module.config' ),
+      main      = require( './routes/client/main' ),
+      sample    = require( './routes/client/sample' ),
+
+      registerUserApi = require( './routes/restApi/API/registerNsignInApi' ),
+      catchAll  = require( './routes' );
+
+  /**
+  ** Configuration File NoSQL Database
+  ***/
+  require( './configuration/mongodb' ); //mongodb integration
+
+  /***
+   ** Start our Express Server
+   ***/
+  var app = node.express();
+
+  /***
+   ** Require our Configuration Files
+   ***/
+  require( './configuration/express' )(app);
+  require( './configuration/passport' )(node.passport);
+
+  /***
+   ** Routes
+   ***/
+  useApp([main,sample,registerUserApi]);
+  //app.use( '/', registerUserApi );
+  app.use( '*', catchAll );
+
+
+  /***
+   ** node.cluster Configuration
+   ***/
+  if (node.cluster.isMaster) {
+    node.clusterService( node );
+  } else {
+    app.listen(app.get('port'), function() {
+      console.log( node.chalk.red.reset.underline('listening to port ') +  node.chalk.cyan.bold((app.get('port'))));
+    });
+  }
+
+  function useApp( param ) {
+    param.forEach(function( name ) {
+      app.use( '/', name );
+    });
+  }
+
+}());
