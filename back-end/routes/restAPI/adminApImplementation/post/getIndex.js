94aebde09e993bae72763d2697f754da5aded73e
canino_jories <canino_jories@hotmail.com>
2015-02-01 20:31:57 +0800

    adding post per department

--- back-end/routes/restAPI/adminApImplementation/post/getIndex.js
+++ back-end/routes/restAPI/adminApImplementation/post/getIndex.js
@@ -22,6 +22,20 @@
   };
 
   exports.getPostDepartmentList = function( req, res, next ) {
+    var query = node.url.parse( req.url ,true).query;
+    node.mongoDB( node, node.config.dbName )
+      .then(function() {
+        node.Post
+          .find({department: query.department})
+          .skip(query.skip)
+          .limit(query.limit)
+          .sort('date', 1)
+          .exec( documents );
 
+          function documents( handleError , documentList ) {
+            if( handleError ) next( handleError );
+            res.status(200).send( documentList );
+          }
+      });
   };
 }());
