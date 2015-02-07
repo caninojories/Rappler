d19a951cf2da76ff550b174995a4f39c58adda95
canino_jories <canino_jories@hotmail.com>
2015-02-07 17:27:50 +0800

    add

--- back-end/routes/restAPI/adminApImplementation/mood/postIndex.js
+++ back-end/routes/restAPI/adminApImplementation/mood/postIndex.js
@@ -4,12 +4,13 @@
   var node = app_require( 'services/module.config' );
 
   exports.postOneMood = function(req, res, next) {
+    var moodUser = req.body.mood;
     node.mongoDB(node, node.config.dbName)
       .then(function() {
         var mood = node.Mood({
           postId: req.body.postId,
           userId: req.body.userId,
-          mood: req.body.mood
+          mood: moodUser
         });
 
         return mood;
@@ -21,47 +22,86 @@
       }).then(function(moodSave, error) {
         node.MoodCount.count({}, function(error, count) {
           if(count === 0) {
-            console.log('count: ' + (count === 0));
-
-              var moodCount = node.MoodCount({
-                happy: 1,
-                sad: 1,
-                annoyed: 1,
-                inspired: 1,
-                afraid: 1
-              });
+              var options;
+              switch(moodUser) {
+                case 'happy':
+                  options = {
+                    happy: 1,
+                    sad: 0,
+                    annoyed: 0,
+                    inspired: 0,
+                    afraid: 0
+                  };
+                  break;
+                case 'sad':
+                  options = {
+                    happy: 0,
+                    sad: 1,
+                    annoyed: 0,
+                    inspired: 0,
+                    afraid: 0
+                  };
+                  break;
+                case 'annoyed':
+                  options = {
+                    happy: 0,
+                    sad: 0,
+                    annoyed: 1,
+                    inspired: 0,
+                    afraid: 0
+                  };
+                  break;
+                case 'inspired':
+                  options = {
+                    happy: 0,
+                    sad: 0,
+                    annoyed: 0,
+                    inspired: 1,
+                    afraid: 0
+                  };
+                  break;
+                case 'afraid':
+                  options = {
+                    happy: 0,
+                    sad: 0,
+                    annoyed: 0,
+                    inspired: 0,
+                    afraid: 1
+                  };
+                  break;
+              }
+              var moodCount = node.MoodCount(options);
               moodCount.save(function(error) {
                 if(error) next(error);
                 res.json('success');
               });
           } else {
             node.MoodCount
-              .findOne({}, function(error, count) {
-                console.log( 'else count: ' + count);
-                count.happy = 2;
-                count.save();
+              .findOne({}, function(error, mood) {
+                console.log( 'else count: ' + mood);
+
+                switch(moodUser) {
+                  case 'happy':
+                    mood.happy = mood.happy + 1;
+                    break;
+                  case 'sad':
+                    mood.sad = mood.sad + 1;
+                    break;
+                  case 'annoyed':
+                    mood.annoyed = mood.annyoed + 1;
+                    break;
+                  case 'inspired':
+                    mood.inspired = mood.inspired + 1;
+                    break;
+                  case 'afraid':
+                    mood.afraid = mood.afraid + 1;
+                    break;
+                }
+
+                mood.save();
               });
           }
         });
-
-            // if(node.ModeCount === undefined) {
-            //   console.log('node ModeCount undefined');
-            //   var moodCount = node.MoodCount({
-            //     happy: 1,
-            //     sad: 1,
-            //     annoyed: 1,
-            //     inspired: 1,
-            //     afraid: 1
-            //   });
-            //   return moodCount;
-            // } else {
-            //   console.log('node ModeCount else');
-            //   node.ModeCount
-            //     .findOne({}, function(error, count) {
-            //       console.log( 'else count: ' + count);
-            //     });
-            // }
-
       });
   };
 }());
