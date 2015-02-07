e338455e97e31f10a6227a3770b020f573c09d5b
canino_jories <canino_jories@hotmail.com>
2015-02-07 20:13:30 +0800

    add

--- back-end/routes/restAPI/API/moodApi.js
+++ back-end/routes/restAPI/API/moodApi.js
@@ -4,16 +4,18 @@
   var node = app_require( 'services/module.config' ),
       app  = node.express(),
 
-      GETONEMOOD  = require('../adminApImplementation/mood/getIndex.js'),
-      GETMOODCOUNT = require('../adminApImplementation/mood/getIndex.js'),
-      POSTONEMOOD = require('../adminApImplementation/mood/postIndex.js');
+      GETONEMOOD    = require('../adminApImplementation/mood/getIndex.js'),
+      GETMOODCOUNT  = require('../adminApImplementation/mood/getIndex.js'),
+      PUTMOOD       = require('../adminApImplementation/mood/putIndex.js'),
+      POSTONEMOOD   = require('../adminApImplementation/mood/postIndex.js');
 
   app.route('/api/mood')
     .get(GETONEMOOD.getOneMood)
     .post(POSTONEMOOD.postOneMood);
 
   app.route('/api/mood/postId')
-    .get(GETMOODCOUNT.getMoodCount);
+    .get(GETMOODCOUNT.getMoodCount)
+    .put(PUTMOOD.putMood);
 
   module.exports = app;
 }());
---
+++ back-end/routes/restAPI/adminApImplementation/mood/putIndex.js
@@ -0,0 +1,22 @@
+(function() {
+  'use strict';
+
+  var node = app_require( 'services/module.config' );
+
+  exports.putMood = function(req, res, next) {
+    console.log( req.body );
+    node.mongoDB(node, node.config.dbName)
+      .then(function() {
+        node.Mood
+          .find({userId: req.body.userId}, result);
+
+        function result(error, mood) {
+          console.log(mood);
+          mood.remove(function(error) {
+            res.json('success');
+          });
+        }
+
+      });
+  };
+}());
