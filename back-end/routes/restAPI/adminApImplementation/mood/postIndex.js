f1a0cce4277f0e4d228a3595eee21e2d5c12219e
canino_jories <canino_jories@hotmail.com>
2015-02-07 16:31:11 +0800

    add

--- back-end/routes/restAPI/adminApImplementation/mood/postIndex.js
+++ back-end/routes/restAPI/adminApImplementation/mood/postIndex.js
@@ -17,23 +17,28 @@
         mood.save(function(err) {
           node.mongoDB(node, node.config.dbName)
             .then(function() {
-              if(node.ModeCount === undefined) {
-                console.log('node ModeCount undefined');
-                var moodCount = node.MoodCount({
-                  happy: 1,
-                  sad: 1,
-                  annoyed: 1,
-                  inspired: 1,
-                  afraid: 1
-                });
-                return moodCount;
-              } else {
-                console.log('node ModeCount else');
-                node.ModeCount
-                  .findOne({}, function(error, count) {
-                    console.log( 'else count: ' + count);
-                  });
-              }
+
+              node.MoodCount.count({}, function(error, count) {
+                console.log('count: ' + count);
+              });
+
+              // if(node.ModeCount === undefined) {
+              //   console.log('node ModeCount undefined');
+              //   var moodCount = node.MoodCount({
+              //     happy: 1,
+              //     sad: 1,
+              //     annoyed: 1,
+              //     inspired: 1,
+              //     afraid: 1
+              //   });
+              //   return moodCount;
+              // } else {
+              //   console.log('node ModeCount else');
+              //   node.ModeCount
+              //     .findOne({}, function(error, count) {
+              //       console.log( 'else count: ' + count);
+              //     });
+              // }
             }).then(function(count, error) {
               console.log('Count Object: ' + count);
               count.save(function(error) {
