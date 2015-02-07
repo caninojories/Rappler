af9d012bc15e81b115134d617e39210d17213ff9
canino_jories <canino_jories@hotmail.com>
2015-02-07 15:25:45 +0800

    mood meter

--- back-end/routes/restAPI/adminApImplementation/mood/postIndex.js
+++ back-end/routes/restAPI/adminApImplementation/mood/postIndex.js
@@ -4,6 +4,7 @@
   var node = app_require( 'services/module.config' );
 
   exports.postOneMood = function(req, res, next) {
+    console.log(req.body);
     node.mongoDB(node, node.config.dbName)
       .then(function() {
         var mood = node.Mood({
@@ -15,21 +16,7 @@
         return mood;
       }).then(function(mood, error) {
         mood.save(function(err) {
-          node.mongoDB(node, node.config.dbName)
-            .then(function() {
-              node.ModeCount
-                .findOne({}, function(error, user) {
-                  user.happy = user.happy + 1;
-                  user.sad   = user.sad + 1;
-                  user.annoyed = user.annoyed + 1;
-                  user.inspired = user.inspired + 1;
-                  user.afraid = user.afraid + 1;
-
-                  user.save(function() {
-                    res.json('success');
-                  });
-                });
-            });
+          res.json('success');
         });
       });
   };
