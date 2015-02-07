c34204ec1ccfbc9370dbbb9263715ef83a19764f
canino_jories <canino_jories@hotmail.com>
2015-02-07 20:45:09 +0800

    add

--- back-end/routes/restAPI/adminApImplementation/mood/deleteIndex.js
+++ back-end/routes/restAPI/adminApImplementation/mood/deleteIndex.js
@@ -5,16 +5,43 @@
 
   exports.deleteMood = function(req, res, next) {
     console.log( req.body );
+    var mood;
     node.mongoDB(node, node.config.dbName)
       .then(function() {
         node.Mood
           .findOne({userId: req.body.userId}, function(error, mood) {
             console.log('mood: ' + mood);
+            mood = mood.mood;
           })
           .remove(result);
 
         function result(mood) {
-          res.json('remove');
+          node.MoodCount
+          .findOne({postId: req.body.postId}, function(error, mood) {
+            console.log( 'else count: ' + mood);
+
+            switch(mood) {
+              case 'happy':
+                mood.happy = mood.happy - 1;
+                break;
+              case 'sad':
+                mood.sad = mood.sad - 1;
+                break;
+              case 'annoyed':
+                mood.annoyed = mood.annyoed - 1;
+                break;
+              case 'inspired':
+                mood.inspired = mood.inspired - 1;
+                break;
+              case 'afraid':
+                mood.afraid = mood.afraid - 1;
+                break;
+            }
+
+            mood.save(function(){
+              res.json('Success: mood vote has been remove');
+            });
+          });
         }
 
       });
