6162ef8cd42b08349d157da1a6a75a2f75a86279
canino_jories <canino_jories@hotmail.com>
2015-02-08 11:44:52 +0800

    add

--- back-end/routes/restAPI/adminApImplementation/mood/getIndex.js
+++ back-end/routes/restAPI/adminApImplementation/mood/getIndex.js
@@ -6,6 +6,12 @@
   exports.getOneMood = function(req, res, next) {
     var query = node.url.parse( req.url ,true).query;
 
+    if (!query.postId) {
+      return res.json('postId is not defined');
+    } else if (!query.userId) {
+      return res.json('userId is not defiined');
+    }
+
     node.mongoDB(node, node.config.dbName)
       .then(function() {
         node.Mood
