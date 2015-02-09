0b2581ccd205c4d574b963010d01a4629bc67b7c
canino_jories <canino_jories@hotmail.com>
2015-02-09 22:33:37 +0800

    add

--- back-end/routes/restAPI/adminApImplementation/comment/postIndex.js
+++ back-end/routes/restAPI/adminApImplementation/comment/postIndex.js
@@ -4,6 +4,10 @@
   var node = app_require( 'services/module.config' );
 
   exports.postOneComment = function(req, res, next) {
+    if (!req.body.postId) {return res.json('postId is undefined plss check your data');}
+    else if (!req.body.userId) {return res.json('userId is undefined plss check your data');}
+    else if (!req.body.content) {return res.json('content is undefined plss check your data');}
+
     node.mongoDB(node, node.config.dbName)
       .then(function() {
         var comment = node.Comment({
