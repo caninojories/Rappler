2f16e425f19dcb7e9bdabcb580ab130070627be8
canino_jories <canino_jories@hotmail.com>
2015-02-23 01:05:56 +0800

    add

--- back-end/services/subscribe.js
+++ back-end/services/subscribe.js
@@ -3,7 +3,7 @@
 
   exports.send = function(node, email, res) {
     var model = {
-      loginUrl :'http://localhost:3000/',
+      postUrl :'http://localhost:3000/',
       title: 'Rappler',
       subTitle: 'Post Subscription',
       body: ''
@@ -17,37 +17,53 @@
         }
     });
 
+
+
     transport(transporter);
 
-    function transport(transporterObject) {
-      var mailOptions = {
-        from: 'caninojories@gmail.com',
-        to: email,
-        subject: 'Rappler Subscription',
-        html: getHtml()
-      };
 
-      sendMail(transporterObject, mailOptions);
+    node._.templateSettings = {
+      interpolate: /\{\{(.+?)\}\}/g
+    };
+
+    function transport(transporterObject, postSubscription, post) {
+        var mailOptions = {
+          from: 'caninojories@gmail.com',
+          to: email,
+          subject: 'Rappler Subscription',
+          html: getHtml(post)
+        };
+
+        sendMail(transporterObject, mailOptions);
+      // var mailOptions = {
+      //   from: 'caninojories@hotmail.com',
+      //   to: postSubscription[i].email,
+      //   subject: 'Account Verification',
+      //   html: getHtml(postId)
+      // };
+      // transporter.sendMail(mailOptions, function(err, info) {
+      //   if(err) {return err;}
+      //   console.log('email sent ' + info.response);
+      //   //res.json('success');
+      // });
     }
 
     function sendMail(transporterObject, mailOptions) {
       transporterObject.sendMail(mailOptions, function(err, info) {
         if(err) {return err;}
         console.log('email sent ' + info.response);
-        res.json('success');
+          res.json('success');
       });
     }
 
-    function getHtml() {
-      var path =  node.path.normalize(__dirname + '/../../') + 'back-end/views/subscribe.html';
+    function getHtml(post) {
+      var path =  node.path.normalize(__dirname + '/../../') + 'back-end/views/postSubscription.html';
       var html = node.fs.readFileSync(path, {'encoding':'utf8'});
 
       var template = node._.template(html);
+      // model.body += post.content;
+      // model.postUrl += post._id;
       return template(model);
     }
-
-    node._.templateSettings = {
-      interpolate: /\{\{(.+?)\}\}/g
-    };
   };
 }());
