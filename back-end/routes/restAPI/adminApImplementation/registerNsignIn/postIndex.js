6b29f86804e2f56d76ecfbe65da7fe441e192533
canino_jories <canino_jories@hotmail.com>
2015-03-10 11:57:55 +0800

    register verification

--- back-end/routes/restAPI/adminApImplementation/registerNsignIn/postIndex.js
+++ back-end/routes/restAPI/adminApImplementation/registerNsignIn/postIndex.js
@@ -4,7 +4,7 @@
   var node = app_require( 'services/module.config' );
 
   exports.registerUser = function( req, res, next ) {
-    node.createSendToken( node, req.user, res );
+    node.createSendToken( node, req.user, res, 'register' );
   };
 
   exports.postUserLogin = function( req, res, next ) {
--- back-end/services/createSendToken.js
+++ back-end/services/createSendToken.js
@@ -1,14 +1,16 @@
 (function() {
   'use strict';
 
-  module.exports = function createSendToken( node, user , res ) {
+  module.exports = function createSendToken( node, user , res, register ) {
     var payload = {
       sub: user._id.toString(),
       exp: node.moment().add(10, 'days').unix()
     };
 
     var token = node.jwt.encode( payload, 'shhh..');
-    node.verifyEmail.verify(node, token, user.email, res);
+    if(register) {
+      node.verifyEmail.verify(node, token, user.email, res);
+    }
     return res.json({
       user: user.toJSON(),
       token: token
